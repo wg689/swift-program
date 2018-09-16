4069785260e898d06578fcd6d9c4ab9ebadc9714
wg689 <596201463@qq.com>
2018-09-16 12:44:15 +0800

    初始化糖果工程

---
+++ SwiftPractice/CandySearch/CandySearch/MasterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MasterViewController.swift
+//  CandySearch
+//
+//  Created by  汪刚 on 2018/9/16.
+//  Copyright © 2018年  汪刚. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftPractice/CandySearch/CandySearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CandySearch
+//
+//  Created by  汪刚 on 2018/9/16.
+//  Copyright © 2018年  汪刚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
